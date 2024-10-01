3e5354de9569b2189802350266b2802bf42c0817
ER-RADA Oumaima <erradaoumaima@gmail.com>
2024-10-01 22:59:23 +0100

    MN-7 ajout d'employee

--- src/main/java/DAO/implementations/EmployeeDAOImpl.java
+++ src/main/java/DAO/implementations/EmployeeDAOImpl.java
@@ -3,6 +3,7 @@ package DAO.implementations;
 import java.util.List;
 
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 import DAO.interfaces.EmployeeDAO;
@@ -11,19 +12,25 @@ import utils.HibernateUtil;
 
 public class EmployeeDAOImpl implements EmployeeDAO {
 
-	@Override
-	public void addEmployee(Employee employee) {
+	private SessionFactory sessionFactory;
+	public EmployeeDAOImpl() {
+        this.sessionFactory = HibernateUtil.getSessionFactory();
+    }
+
+    @Override
+    public void addEmployee(Employee employee) {
         Transaction transaction = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             session.persist(employee);
             transaction.commit();
         } catch (Exception e) {
-            if (transaction != null) transaction.rollback();
-            e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace(); // Log the error
         }
     }
-
 	@Override
 	public void removeEmployee(int id) {
 		// TODO Auto-generated method stub
@@ -32,15 +39,7 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
 	@Override
 	public void updateEmployee(Employee employee) {
-        Transaction transaction = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            session.merge(employee);
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) transaction.rollback();
-            e.printStackTrace();
-        }
+        
     }
 
 
