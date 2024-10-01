d4dd526096c2cb50aa110bd4b159cf73f09a8efa
ER-RADA Oumaima <erradaoumaima@gmail.com>
2024-10-01 21:51:40 +0100

    MN-6 Configurer Hibernate avec PostgreSQL

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,31 @@
+package main;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import models.Employee;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        
+        Employee empl = new Employee();
+        empl.setName("oumaima");
+        empl.setEmail("email@gmail.com");
+        empl.setPhone("0694253502");
+        empl.setDepartment("info");
+        empl.setPosition("consultant");
+        
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        
+        session.persist(empl);
+        
+        transaction.commit();
+        session.close(); 
+        sessionFactory.close(); 
+    }
+}
