4d6ae227217fe83992918870931947b6b8677194
ER-RADA Oumaima <erradaoumaima@gmail.com>
2024-10-01 21:55:20 +0100

    MN-20 Creer model Employee

---
+++ src/main/java/models/Employee.java
@@ -0,0 +1,58 @@
+package models;
+
+public class Employee {
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+    private String department;
+    private String position;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}
